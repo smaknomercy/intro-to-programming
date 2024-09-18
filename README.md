ae45e175b7c362fa49ff3507493fa7a4a072e90d
smaknomercy <slaviksmak@gmail.com>
2024-09-18 22:47:15 +0300

    Update readme and add new main.c | Completed home task

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # intro-to-programming
 My repository to course "Introduction to programming"
+Yaroslav Smakorovskyi
+Home task #1
+Variant #3
+3.1 Count down from N-1 to 0 (inclusive), call each number you count "y" and Count from 0 to y (inclusive), call each number you count "x" and 
+	if (x + y == (N-1)) 
+		then place a * (asterisk) at (x, y) 
+	and elseif (x == y ) 
+		then place a * (asterisk) at (x, y) 
+	otherwise place a | (vertical bar) at (x, y)
+
+3.2 Summarized the content in a table. Run main.c to see it
---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  printf("Article title: The fundamental types in C++\n");
+  printf("Article URL: https://en.cppreference.com/w/cpp/language/types\n");
+  printf("Summary table:\n");
+  printf("  | Name | Size (bytes) |\n");
+  printf("  |---|---| \n");
+  printf("  | bool | 1 |\n");
+  printf("  | char | 1 |\n");
+  printf("  | short | 2 |\n");
+  printf("  | int | 4 |\n");
+  printf("  | long | 4 or 8 |\n");
+  printf("  | long long | 8 |\n");
+  printf("  | float | 4 |\n");
+  printf("  | double | 8 |\n");
+  printf("  | long double | 8 or 10 |\n");
+  return 0;
+}
