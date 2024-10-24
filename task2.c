f0121c84525b2b7ace7eb98171f11d81d99f46f2
Yaroslav <60614340+smaknomercy@users.noreply.github.com>
2024-10-24 14:36:39 +0300

    Update task2.c
    Update advanced task2.c and now it works correctly

--- task2.c
+++ task2.c
@@ -8,20 +8,37 @@
 int main() {
     int numbers[N];
     int occurrences[RANGE + 1] = {0};
-    int i, j;
+    int indices[RANGE + 1];
+    int i, j, temp;
 
     srand(time(NULL));
+
     for (i = 0; i < N; i++) {
         numbers[i] = rand() % RANGE + 1;
         occurrences[numbers[i]]++;
     }
-    for (i = RANGE; i >= 1; i--) {
-        if (occurrences[i] > 0) {
-            printf("Number %d: %d times\n", i, occurrences[i]);
+
+    for (i = 0; i <= RANGE - 1; i++) {
+        indices[i] = i;
+    }
+
+    for (i = 0; i <= RANGE - 1; i++) {
+        for (j = 1; j <= RANGE - i; j++) {
+            if (occurrences[indices[j]] < occurrences[indices[j + 1]]) {
+                temp = indices[j];
+                indices[j] = indices[j + 1];
+                indices[j + 1] = temp;
+            }
         }
     }
+
+    for (i = 0; i <= RANGE - 1; i++) {
+        if (occurrences[indices[i]] > 0) {
+            printf("Number %d: %d times\n", indices[i], occurrences[indices[i]]);
+        }
+    }
+
     return 0;
 }
-
-//Time Complexity: O(N)
-//Space Complexity: O(N)
+//Time Complexity: O(N^2)
+//Space Complexity: O(N^2)
